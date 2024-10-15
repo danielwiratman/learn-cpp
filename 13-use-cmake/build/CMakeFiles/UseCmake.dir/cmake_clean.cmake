d9b6de586e0198441ad32134e93a74ac8e6e03b0
Daniel Wiratman <daniel@equnix.biz>
2024-10-15 22:57:16 +0700

    [13] leaern cmake

---
+++ 13-use-cmake/build/CMakeFiles/UseCmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/daniel/study/cpp/13-use-cmake/UseCmake"
+  "/home/daniel/study/cpp/13-use-cmake/UseCmake.pdb"
+  "CMakeFiles/UseCmake.dir/src/main.cpp.o"
+  "CMakeFiles/UseCmake.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UseCmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
