6e2e8e0f3cdc1cff063b3c258efc7ab3c0c1261c
Daniel Wiratman <danielwiratman@gmail.com>
2024-10-17 20:27:06 +0700

    [leetcode] 88

--- leetcode/88-merge-sorted-arr/main.cpp
+++ leetcode/88-merge-sorted-arr/main.cpp
@@ -11,6 +11,25 @@ class Solution
 	void
 	merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 	{
+		int i = m + n - 1;
+		int j = m - 1;
+		int k = n - 1;
+
+		while (k > -1)
+		{
+			if (j > -1 && nums1[j] > nums2[k])
+			{
+				nums1[i] = nums1[j];
+				i--;
+				j--;
+			}
+			else
+			{
+				nums1[i] = nums2[k];
+				i--;
+				k--;
+			}
+		}
 	}
 };
 
